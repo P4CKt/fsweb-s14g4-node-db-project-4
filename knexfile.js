ea4a6917a12af470ccba38a6d179f9509b7be69f
P4CKt <omurcanuslu@outlook.com>
2023-03-30 13:12:05 +0300

    first setup

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
+  // this enables foreign keys in SQLite
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+  },
+};
